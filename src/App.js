a65d08e8ca5c63fa8ecaa4417d2fe20e928b8716
Mbibby95 <mattyboysayshi@gmail.com>
2019-03-17 00:09:28 +0000

    Layout Completed, Initial click and match handler implemented

--- src/App.js
+++ src/App.js
@@ -1,52 +1,81 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function EmojiCard(props) {
-  return (
-    <div className="EmojiCard">{props.emoji}</div>
-  );
+ class EmojiCard extends Component {
+
+  constructor(props){
+    super(props);
+    this.handleSelected = this.handleSelected.bind(this);
+  }
+
+  handleSelected = () => {
+    this.props.onEmojiSelected(this.props.arrpos);
+  }
+
+  render() {
+    return (
+      <div className="EmojiCard" onClick={this.handleSelected}>{this.props.emoji}</div>
+    );
+  }
+}
+
+class EmojiRow extends Component {
+
+  createRow = (arr, arrstart) => {
+
+    let row = [];
+  
+    for (let index = 0; index < arr.length; index++) {
+      row.push(<EmojiCard key={index+arrstart} emoji={arr[index]} 
+        arrpos={index+arrstart} onEmojiSelected={this.props.onEmojiSelected}></EmojiCard>);
+    }
+
+    return row;
+  }
+
+  render(){
+    return(
+      this.createRow(this.props.emojis, this.props.arrstart)
+    );
+  }
+
 }
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      emojigrid : this.props.em1,
+      matched : new Array(30).fill(false),
+      prevclick: 0
+    }
+    this.onEmojiSelected = this.onEmojiSelected.bind(this);
+  }
+
+  onEmojiSelected = (id) => {
+    if (this.state.emojigrid[this.state.prevclick] === this.state.emojigrid[id]) {
+      console.log("WINNER WINNER")
+    }
+    this.setState({
+      prevclick : id
+    });
+    console.log(id);
+  }
+
   render() {
     return (
       <div className="Wrapper">
         {/* ROW 1 */}
-        <EmojiCard emoji="💸" />
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
+        <EmojiRow onEmojiSelected={this.onEmojiSelected} arrstart={0} emojis={this.props.em1.slice(0,6)} />
         {/* ROW 2 */}
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
+        <EmojiRow onEmojiSelected={this.onEmojiSelected} arrstart={6} emojis={this.props.em1.slice(6,12)} />
         {/* ROW 3 */}
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
+        <EmojiRow onEmojiSelected={this.onEmojiSelected} arrstart={12} emojis={this.props.em1.slice(12,18)} />
         {/* ROW 4 */}
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/> 
-        <EmojiCard emoji="💸"/>
+        <EmojiRow onEmojiSelected={this.onEmojiSelected} arrstart={18} emojis={this.props.em1.slice(18,24)} />
         {/* ROW 5 */}
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>         
-        {/* ROW 6 /*/}
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>
-        <EmojiCard emoji="💸"/>       
+        <EmojiRow onEmojiSelected={this.onEmojiSelected} arrstart={24} emojis={this.props.em1.slice(24,30)} />    
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { shuffle } from 'underscore'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const emojis = ["😀","😂","😭","🤓","🤩","👩‍💻","🕵️‍","💂‍","👍",
+                "👏","👌","🐶","🐳","🌞","🍺"]
+
+var doublemojis = emojis.reduce(function(result, current) {
+        return result.concat([current,current]);
+    }, []
+)
+
+var shuffled = shuffle(doublemojis);
+
+console.log(shuffled);
+
+ReactDOM.render(<App em1={shuffled}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
